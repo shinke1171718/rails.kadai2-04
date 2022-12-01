a7588684209a9a9168d84e4dc9c261e724bdd9ba
Apple <apple@ShinkenoMacBook.local>
2022-12-01 20:11:57 +0900

    userのコントローラーを作成

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   def after_sign_in_path_for(resource)
-    "(ログイン後に遷移させたいページ)"
+    user_path(resource)
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   devise_scope :user do
     root "users/sessions#new"
   end
+
+  resources :users, only: [:show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
