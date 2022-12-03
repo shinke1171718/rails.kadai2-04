7974482303ee65a65f911cd92df982d96888691f
Apple <apple@ShinkenoMacBook.local>
2022-12-03 19:47:05 +0900

    roomモデル削除(マイグレーションファイル削除できていない)

---
+++ db/migrate/20221203094310_removeuser_from_rooms.rb
@@ -0,0 +1,4 @@
+class RemoveuserFromRooms < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_11_30_110725) do
+ActiveRecord::Schema.define(version: 2022_12_03_094310) do
+
+  create_table "rooms", force: :cascade do |t|
+    t.string "name"
+    t.text "Introduction"
+    t.integer "price"
+    t.string "address"
+    t.integer "user_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_rooms_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -24,4 +35,5 @@ ActiveRecord::Schema.define(version: 2022_11_30_110725) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "rooms", "users"
 end
